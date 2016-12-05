44f848a5cecd0eed3c11377d44cadfa7e212eab5
ILUXONCHIK <iluxon4ik@hotmail.com>
2016-12-05 07:38:41 +0000

    chore: additions to protocol definiton and documentation

--- server/bluetooth/protocol.py
+++ server/bluetooth/protocol.py
@@ -1,3 +1,29 @@
 class Protocol(object):
-    ENCRYPT = b'enc'
+    """
+    Message sturcture:
+        * ENCRYPT: username||pdw||token
+        * DECRYPT: username||pdw||token
+        * PWD_LOGIN: username||pwd
+        
+        * DECRYPTION_ERR: file_path||msg
+        * NEW_TOKEN: token
+
+    Messages which format has not been sepecified above do not carry any data,
+    consisting only of the byte tag defined below.
+
+    After receiving PWD_LOGIN, the server replies with a token (i.e.
+    sends NEW_TOKEN message). The client app has to update the token it has
+    stored locally.
+    """
+
+    # messages client->server
+    ENCRYPT = b'enc'  # NOTE: client doens't have to send this directly
     DECRYPT = b'dec'
+    PWD_LOGIN = b'log'
+
+    # messages server->client
+    PWD_LOGIN_ERR = b'log_err'
+    TOKEN_EXPIRED_ERR = b'tok_exp_err'
+    TOKEN_WRONG_ERR = b'tok_wrong_err'
+    DECRYPTION_ERR = b'dec_err'  # error in a decryption of a file
+    NEW_TOKEN = b'tok_new'  # commands the client to update the token
